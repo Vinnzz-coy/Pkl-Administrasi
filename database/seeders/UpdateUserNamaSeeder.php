d39420fe152370bc0c80963adf264152cfce3091
Napisss <irsyadnafis73@gmail.com>
2025-12-11 11:23:31 +0700

    update user

---
+++ database/seeders/UpdateUserNamaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UpdateUserNamaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Daftar NIP dan Nama Admin
+        $admins = [
+            '1987654321' => 'Admin Satu',
+            '1976543210' => 'Admin Dua',
+            '1965432109' => 'Admin Tiga',
+            '1954321098' => 'Admin Empat',
+            '1943210987' => 'Admin Lima',
+        ];
+
+        foreach ($admins as $nip => $nama) {
+            DB::table('users')->where('nip', $nip)->update([
+                'nama' => $nama,
+            ]);
+        }
+    }
+}
